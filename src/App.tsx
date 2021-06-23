9b022405c28e6f73aa3d9a04001f9043ec2e37ec
joaovitor <joaobuarqueestudos@gmail.com>
2021-06-22 22:08:48 -0300

    nlw dia 2 React

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import { Button } from './components/Button'
+import { Route, BrowserRouter } from 'react-router-dom';
+
+import { NewRoom } from './pages/NewRoom';
+import { Home } from './pages/Home'
+
+import { AuthContextProvider } from './contexts/AuthContext'
+
 function App() {
   return (
-   <div>
-     <Button />
-     <Button />
-     <Button />
-     <Button />
-     <Button />
-     <Button />
-   </div>
+    <BrowserRouter>
+        <AuthContextProvider>
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" component={NewRoom}/>
+        </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
